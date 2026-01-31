3eacdeccc4c1fc846747ac9ca03dcceffe53dfee
Nguyen Tien Dat <tie.dat2004@gmail.com>
2026-01-31 23:21:05 +0700

    initial commit

---
+++ oulib-backend/src/main/java/com/ou/oulib/OulibBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ou.oulib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OulibBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OulibBackendApplication.class, args);
+	}
+
+}
---
+++ oulib-backend/src/test/java/com/ou/oulib/OulibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou.oulib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OulibBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
